e7fb1f2e9c15cb7853eef7dc4fcbe7179abd43b2
Akansha Kumari <guptaakansha3004@gmail.com>
2025-02-18 22:51:32 +0530

    Project Completed

---
+++ frontend/src/App.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react';
+import Navbar from './components/Navbar';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import ProfilePage from "./pages/ProfilePage";
+import SettingsPage from "./pages/SettingsPage";
+import { useAuthStore } from './store/useAuthStore';
+import { useThemeStore } from './store/useThemeStore';
+import { Loader } from 'lucide-react';
+import { Toaster } from 'react-hot-toast';
+
+const App = () => {
+  const { authUser, checkAuth, onlineUsers} = useAuthStore();
+  const [isCheckingAuth, setIsCheckingAuth] = useState(true);
+  const {theme} = useThemeStore();
+
+  console.log(onlineUsers);
+
+  useEffect(() => {
+    const verifyAuth = async () => {
+      await checkAuth();
+      setIsCheckingAuth(false); // ✅ Set to false after checking
+    };
+    verifyAuth();
+  }, [checkAuth]);
+
+  console.log({ authUser });
+
+  // ✅ Now isCheckingAuth is defined and used safely
+  if (isCheckingAuth && !authUser) return (
+    <div className="flex items-center justify-center h-screen">
+      <Loader className="h-10 w-10 animate-spin" />
+    </div>
+  );
+
+  return (
+    <div data-theme={theme}>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
+        <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
+        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
+      </Routes>
+
+      <Toaster />
+    </div>
+  );
+};
+
+export default App;
